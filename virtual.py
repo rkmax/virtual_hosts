bd3089df553ba255bcca7dfd0be3d9a47b94dd78
Julian Reyes Escrigas <julian.reyes.es@gmail.com>
2011-06-13 13:22:14 -0500

    añadido validacion host existente

--- virtual.py
+++ virtual.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 from optparse import OptionParser
 import os
 
 parser = OptionParser()
+path_apache_vhost = '/etc/apache2/sites-available/{0}'
+path_sys_hosts = '/etc/hosts'
 
 parser.add_option( "-d", "--directory", dest="directory", help="The path to the directory", metavar="DIRECTORY" )
-
 parser.add_option( "-n", "--name", dest="name", help="Name of the virtual host", metavar="NAME" )
 
 (options, args) = parser.parse_args()
@@ -15,13 +16,25 @@ template = open( 'template', 'r' )
 virtualHostText = template.read()
 template.close()
 
-virtualHostText = virtualHostText.format( name = options.name, directory = options.directory )
 
-fileNameVirtualHost = '/etc/apache2/sites-available/{0}.conf'.format(options.name)
+# leer el archivo host
+fileHosts = open(path_sys_hosts, 'r')
+text_host = fileHosts.read();
+fileHosts.close()
+
+# comprueba que no exista la entrada en el archivo host
+if text_host.find("{0}".format(options.name)) < 0:
+  fileHosts = open(path_sys_hosts, 'a')
+  fileHosts.write("127.0.0.1 {0}".format(options.name))
+  fileHosts.close()
+
+virtualHostText = virtualHostText.format( name = options.name,  directory = options.directory )
+
+fileNameVirtualHost = path_apache_vhost.format(options.name)
 fileVirtualHost = open( fileNameVirtualHost, 'w' )
 fileVirtualHost.write( virtualHostText )
 fileVirtualHost.close()
 
-os.symlink( fileNameVirtualHost, '/etc/apache2/sites-enabled/{0}.conf'.format(options.name) )
+os.system('a2ensite {0}'.format(options.name))
 
-os.system('/etc/init.d/apache2 restart')
+os.system('service apache2 restart')
